975078466f3d14ae6e2c15efb0ebd4a6e2cbbbf5
Ken Moore <ken@pcbsd.org>
2014-04-07 09:29:14 -0400

    Make sure we keep the "." between display and screen numbers when setting the background.

--- Lumina-DE/src/lumina-desktop/LDesktop.cpp
+++ Lumina-DE/src/lumina-desktop/LDesktop.cpp
@@ -111,7 +111,7 @@ void LDesktop::UpdateBackground(){
   if( (bgFile.toLower()=="default")){ bgFile = "/usr/local/share/Lumina-DE/desktop-background.jpg"; }
   //Now set this file as the current background
   QString display = QString( getenv("DISPLAY") );
-  display = display.section(".",0,0)+desktopnumber; //only the current screen
+  display = display.section(".",0,0)+"."+desktopnumber; //only the current screen
   QString cmd = "xv +24 -maxp -display "+display+" -rmode 5 -quit \""+bgFile+"\"";
   QProcess::startDetached(cmd);
   //Now reset the timer for the next change (if appropriate)
