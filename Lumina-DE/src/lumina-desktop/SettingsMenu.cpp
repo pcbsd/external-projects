840761bf9f070d3e199b1bddbbf912d4e591c267
Ken Moore <ken@pcbsd.org>
2014-04-15 11:05:38 -0400

    oops, forgot to connect the signal/slot for the settings menu in the last commit.

--- Lumina-DE/src/lumina-desktop/SettingsMenu.cpp
+++ Lumina-DE/src/lumina-desktop/SettingsMenu.cpp
@@ -3,6 +3,7 @@
 SettingsMenu::SettingsMenu() : QMenu(){
   this->setTitle( tr("Desktop Settings") );
   this->setIcon( LXDG::findIcon("configure","") );
+  connect(this, SIGNAL(triggered(QAction*)), this, SLOT(runApp(QAction*)) );
   //Now setup the possible configuration options
   QAction *act = new QAction(LXDG::findIcon("preferences-desktop-screensaver",""), tr("Screensaver"), this);
 	act->setWhatsThis("xscreensaver-demo");
